1c19ae1b84787c08e9801e7505a9666b68ea1de0
PCupboard <PatrickJemieljanczyk@gmail.com>
2025-02-12 17:51:38 +0100

    Drøfter hvordan jeg skal gjøre items (våpen, rustning, potions, etc).

--- src/model/Item.java
+++ src/model/Item.java
@@ -5,4 +5,25 @@ public class Item {
     Thought of as a general class for all items.
     Items include stuff like potions, weapons, armor, 
      */
+    private int id;
+    private String name;
+    private String description;
+    private double attack;
+    private double defence;
+
+    public void Item(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    // Constructors
+    public void Item(int id, String name, String description, double attack, double defence) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.attack = attack;
+        this.defence = defence;
+    }
+
 }
--- src/model/Weapon.java
+++ src/model/Weapon.java
@@ -1,8 +1,6 @@
 package model;
 
 public class Weapon extends Item {
-    private int id;
-    private String name;
     private int rarity;
     private int damage;
     private boolean two_handed;
